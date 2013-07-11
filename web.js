61a75224a5e6747bf3fb8b0ff4d90d860ffb86e6
karansj <karan14_johar@yahoo.com>
2013-07-11 18:10:14 +0530

    Update web.js
    Buffer

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync("index.html").toString('utf-8');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
